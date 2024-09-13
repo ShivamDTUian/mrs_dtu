d46856aa1821b541ceec3467e8cdffd0df11c748
ShivamDTUian <shivam109.m@gmail.com>
2024-09-13 20:32:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ import pandas as pd
 import requests
 import os
 
-st.write("Files in current directory:", os.listdir())
-movie_dict_path = 'Moviw_recommendation_sys/movie_dict.pkl'
-similarity_path = 'Moviw_recommendation_sys/similarity.pkl'
-
+movie_dict_path = os.path.join('Moviw_recommendation_sys', 'movie_dict.pkl')
+similarity_path = os.path.join('Moviw_recommendation_sys', 'similarity.pkl')
 
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=f53ead6221d84221e10915757e84d727&language=en-US'.format(movie_id))
@@ -38,60 +36,40 @@ def Recommend(movie):
     for i in movie_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append((movies.iloc[i[0]].title).upper())
-        
         recommended_movies_poster.append(fetch_poster(movie_id))
         recommended_movie_url.append(fetch_movie_url(movie_id))
         
-    return recommended_movies, recommended_movies_poster, recommended_movie_url  # Also return posters
+    return recommended_movies, recommended_movies_poster, recommended_movie_url  
 
-try:
-    movie_dict = pickle.load(open(movie_dict_path, 'rb'))
-    similarity = pickle.load(open(similarity_path, 'rb'))
-except FileNotFoundError as e:
-    st.error(f"File not found: {e}")
-    st.stop()
+movie_dict = pickle.load(open(movie_dict_path, 'rb'))
+similarity = pickle.load(open(similarity_path, 'rb'))
 movies = pd.DataFrame(movie_dict)
 
 st.title('Movie Recommender System')
 
 selected_movie_name  = st.selectbox(
-"How would you like to be contacted?",
-movies['title'].values,)
+"How would you like to be contacted?",movies['title'].values,)
 
 st.write("You Selected:", (selected_movie_name).upper())
 
 if st.button('Recommend'):
-    name, poster, url = Recommend(selected_movie_name)
+    names, posters, urls = Recommend(selected_movie_name)
 
     
     col1, col2, col3, col4, col5 = st.columns(5)  # Create 5 columns
+    cols = [col1, col2, col3, col4, col5]
     
-    with col1:
-        st.markdown(f"[![{name[0]}]({poster[0]})]({url[0]})")
-
-    with col2:
-        st.markdown(f"[![{name[1]}]({poster[1]})]({url[1]})")
-        
-    with col3:
-        st.markdown(f"[![{name[2]}]({poster[2]})]({url[2]})")
-        
-    with col4:
-        st.markdown(f"[![{name[3]}]({poster[3]})]({url[3]})")
-
-    with col5:
-       st.markdown(f"[![{name[4]}]({poster[4]})]({url[4]})")
-        
+    for idx, col in enumerate(cols):
+        if idx < len(names):
+            with col:
+                st.markdown(f"**{names[idx]}**")  # Display movie name
+                st.markdown(f"[![Poster]({posters[idx]})]({urls[idx]})")  # Display poster with URL
+    
     col6, col7, col8, col9, col10 = st.columns(5)
+    cols = [col6, col7, col8, col9, col10]
     
-    with col6:
-        st.markdown(f"[![{name[5]}]({poster[5]})]({url[5]})")
-        
-    with col7:
-       st.markdown(f"[![{name[6]}]({poster[6]})]({url[6]})")
-    with col8:
-        st.markdown(f"[![{name[7]}]({poster[7]})]({url[7]})")
-        
-    with col9:
-        st.markdown(f"[![{name[8]}]({poster[8]})]({url[8]})")
-    with col10:
-        st.markdown(f"[![{name[9]}]({poster[9]})]({url[9]})")
+    for idx, col in enumerate(cols, start=5):
+        if idx < len(names):
+            with col:
+                st.markdown(f"**{names[idx]}**")  
+                st.markdown(f"[![Poster]({posters[idx]})]({urls[idx]})")
